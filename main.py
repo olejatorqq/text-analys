6c3287ddecb3475b9a3b2878d006d0551d282047
olejatorqq <oleg.orlowsky@gmail.com>
2023-03-28 14:17:43 +0300

    Added word normalization and stop words

--- main.py
+++ main.py
@@ -15,10 +15,13 @@ from nltk.corpus import stopwords
 from wordcloud import WordCloud
 import matplotlib.pyplot as plt
 
+# needed for word normalization
+import pymorphy2
+
 # FOR README - stopwords and punkt
 
 def format_text():
-    f = open('text-for-analys/1.Гаррии_Поттер_и_философский_камень.txt', 'r', encoding='utf-8')
+    f = open('text-for-analys/1.Гарри_Поттер_и_философский_камень.txt', 'r', encoding='utf-8')
     text = f.read()
     text = text.lower()
     spec_chars = string.punctuation + '«»\t—…’'
@@ -32,15 +35,24 @@ def format_text():
     
     # Tokenization text
     text_tokens = word_tokenize(text)
-    text = nltk.Text(text_tokens)
+
+    morph = pymorphy2.MorphAnalyzer()
+    filtered_tokens = []
+
+    # Get normalize for of current word
+    for token in text_tokens:
+        p = morph.parse(str(token))[0]
+        filtered_tokens.append(p.normal_form)
+
+    text = nltk.Text(filtered_tokens)
 
     # Countiong words by the popularity
     tdist = FreqDist(text)
 
     # Formation stop words and scrubbing them
     stop_words = stopwords.words("russian")
-    stop_words.extend(['–', 'это'])
-    text_tokens = [token.strip() for token in text_tokens if token not in stop_words]
+    stop_words.extend(['это', 'всё', 'который', 'сказать', 'свой', 'еще', 'её', 'видеть', 'говорить', 'думать', 'сказать', 'весь'])
+    text_tokens = [token.strip() for token in filtered_tokens if token not in stop_words]
     
     text = nltk.Text(text_tokens)
     tdist_sw = FreqDist(text)
